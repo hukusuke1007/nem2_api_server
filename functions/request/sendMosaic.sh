e364aed67c760e2807a39b5a12dbf350525f671d
Shohei Nakagawa <hukusuke1007@gmail.com>
2019-12-14 09:18:44 +0900

    update request shell

--- functions/request/sendMosaic.sh
+++ functions/request/sendMosaic.sh
@@ -1,6 +1,5 @@
 
 API="http://localhost:5000/nem2-wallet-d3f63/asia-northeast1/v1-router-catapult-api"
-ENDPOIT="https://fushicho2-fee.opening-line.jp:3001"
 
 echo "--- Request ---"
 curl -w '\n' -X POST $API/sendMosaic \
