6c0b6e8cc5debd7a194d5d20034feaf67ba05bf2
Shohei Nakagawa <hukusuke1007@gmail.com>
2019-12-14 09:26:44 +0900

    update requset shell

--- functions/request/address.sh
+++ functions/request/address.sh
@@ -1,5 +1,3 @@
-
-API="http://localhost:5000/nem2-wallet-d3f63/asia-northeast1/v1-router-catapult-api"
 ENDPOINT="https://fushicho2-fee.opening-line.jp:3001"
 TO_ADDRESS="SCGNCYA2FSBSYI36KC2ZCE6ILPDFOUU6XCUFJMA4"
 
--- functions/request/faucet.sh
+++ functions/request/faucet.sh
@@ -1,4 +1,3 @@
-
 API="http://localhost:5000/nem2-wallet-d3f63/asia-northeast1/v1-router-catapult-api"
 
 echo "--- Request ---"
